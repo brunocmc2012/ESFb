68cdd7a27f1181723a54a715dbdd9f5abb50a880
brunocmc2012 <brunocmc2012@gmail.com>
2023-10-11 13:23:58 -0300

    feat(dev): solving problems with facebook auth --wip

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,20 @@
 import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional (Esse é meu db.firebase pessoal. Usar config.firebase já criada no projeto no lugar dessa!)
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyAVuAKTO3J70x4iRf_bP6orT1yB3te6zd0",
+  authDomain: "esfb-888e5.firebaseapp.com",
+  projectId: "esfb-888e5",
+  storageBucket: "esfb-888e5.appspot.com",
+  messagingSenderId: "195277464453",
+  appId: "1:195277464453:web:090de1beaad39d62175aec"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
 
 const db = app.firestore();
 const storage = firebase.storage();
-const auth = app.auth();
 
-export { auth, db, storage };
+export { db, storage };
